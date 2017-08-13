16f223692e047d7100bc6d170d5ea573cea57a16
명세교 <myungsekyo@gmail.com>
2017-08-13 16:32:09 +0900

    auto login implemented

--- app/src/main/java/com/example/sekyo/mongddangseoul/LoginActivity.java
+++ app/src/main/java/com/example/sekyo/mongddangseoul/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.sekyo.mongddangseoul;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -25,14 +28,26 @@ import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     public static String address = "http://sekyo.cafe24app.com";
+
     Button loginButton;
     Button facebookLoginButton;
     Button signUpButton;
     EditText idText;
     EditText pwText;
+    CheckBox autoLogin;
+
+    SharedPreferences sharedPreferences;
+
     private CallbackManager callbackManager;
     private LoginManager loginManager;
     private List<String> permissionNeeds = Arrays.asList("email");
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +59,10 @@ public class LoginActivity extends AppCompatActivity {
         signUpButton = (Button) findViewById(R.id.sign_up_btn);
         idText = (EditText) findViewById(R.id.login_id);
         pwText = (EditText) findViewById(R.id.login_pw);
+        autoLogin = (CheckBox) findViewById(R.id.checkBox);
 
+
+        //////////////////////////////////FACEBOOK//////////////////////////////////////////////////
         FacebookSdk.sdkInitialize(this.getApplicationContext());
         if (BuildConfig.DEBUG) {
             FacebookSdk.setIsDebugEnabled(true);
@@ -98,6 +116,29 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        //Auto Login check
+        sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
+
+        autoLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.remove("id");
+                    editor.remove("pw");
+                    editor.commit();
+                }
+            }
+        });
+
+        if (sharedPreferences.getString("id", null) != null) {
+            idText.setText(sharedPreferences.getString("id", null));
+            pwText.setText(sharedPreferences.getString("pw", null));
+            autoLogin.setChecked(true);
+        }
+
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,25 +153,33 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Ion.with(getApplicationContext())
                         .load(address + "/sign_in")
-                        .setMultipartParameter("id",idText.getText().toString())
-                        .setMultipartParameter("pw",pwText.getText().toString())
+                        .setMultipartParameter("id", idText.getText().toString())
+                        .setMultipartParameter("pw", pwText.getText().toString())
                         .asString()
                         .setCallback(new FutureCallback<String>() {
                             @Override
                             public void onCompleted(Exception e, String result) {
-                                if(e == null) {
-                                    if(result.equals("ok")){
+                                if (e == null) {
+                                    if (result.equals("ok")) {
+                                        if(autoLogin.isChecked()) {
+                                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                                            editor.putString("id", idText.getText().toString());
+                                            editor.putString("pw", pwText.getText().toString());
+                                            editor.commit();
+                                        }
                                         LoginInfo.id = idText.getText().toString();
                                         LoginInfo.pw = pwText.getText().toString();
+
                                         Toast.makeText(getApplicationContext(), "로그인되었습니다.", Toast.LENGTH_LONG).show();
+
                                         Intent intent = new Intent(getBaseContext(), HomeActivity.class);
                                         startActivity(intent);
                                         finish();
-                                    } else if (result.equals("no_id")){
+                                    } else if (result.equals("no_id")) {
                                         Toast.makeText(getApplicationContext(), "존재하지 않는 아이디입니다.", Toast.LENGTH_LONG).show();
-                                    } else if (result.equals("wrong_pw")){
+                                    } else if (result.equals("wrong_pw")) {
                                         Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
-                                    } else if (result.equals("db_error")){
+                                    } else if (result.equals("db_error")) {
                                         Toast.makeText(getApplicationContext(), "서버 데이터베이스에 문제가 발생했습니다.", Toast.LENGTH_LONG).show();
                                     }
                                 } else {
@@ -143,10 +192,4 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-
-    }
 }
